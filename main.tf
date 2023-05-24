be70785dd90673cffb7f57370df30b97c8a04189
pavanbairu <pavan.bairu1@gmail.com>
2023-05-24 23:59:45 +0530

    creating the NAT gateway

--- main.tf
+++ main.tf
@@ -31,15 +31,10 @@ resource "aws_eip" "ngw" {
   tags =  merge(var.tags, { Name = "${var.env}-ngw"})
 }
 
-#resource "aws_nat_gateway" "example" {
-#  allocation_id = aws_eip.example.id
-#  subnet_id     = aws_subnet.example.id
-#
-#  tags = {
-#    Name = "gw NAT"
-#  }
-#
-#  # To ensure proper ordering, it is recommended to add an explicit dependency
-#  # on the Internet Gateway for the VPC.
-#  depends_on = [aws_internet_gateway.example]
-#}
+resource "aws_nat_gateway" "example" {
+  count = length(lookup(lookup(var.subnets, "public", null), "cidr_block", 0))
+  allocation_id = aws_eip.ngw[count.index].id
+  subnet_id     = module.subnets["public"].subnet_ids[count.index]
+
+  tags =  merge(var.tags, { Name = "${var.env}-ngw"})
+}
