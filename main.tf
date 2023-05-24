9ad5e59c3a8870bfa0d468e13671eaabe07d325b
pavanbairu <pavan.bairu1@gmail.com>
2023-05-24 23:50:28 +0530

    creating the elastic ip

--- main.tf
+++ main.tf
@@ -23,3 +23,23 @@ resource "aws_internet_gateway" "igw" {
 
   tags =  merge(var.tags, { Name = "${var.env}-igw"})
 }
+
+resource "aws_eip" "ngw" {
+#  count = length(lookup(lookup(var.subnets, "public", null), "cidr_block", 0))
+  count = length(var.subnets["public"].cidr_block)
+  vpc      = true
+  tags =  merge(var.tags, { Name = "${var.env}-ngw"})
+}
+
+#resource "aws_nat_gateway" "example" {
+#  allocation_id = aws_eip.example.id
+#  subnet_id     = aws_subnet.example.id
+#
+#  tags = {
+#    Name = "gw NAT"
+#  }
+#
+#  # To ensure proper ordering, it is recommended to add an explicit dependency
+#  # on the Internet Gateway for the VPC.
+#  depends_on = [aws_internet_gateway.example]
+#}
